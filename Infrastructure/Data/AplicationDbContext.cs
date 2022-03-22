c234210c82f6879847720f6e47251d9cc86c86cd
Luciano <feyaluciano@gmail.com>
2022-03-21 21:25:03 -0300

    to fix error

--- Infrastructure/Data/AplicationDbContext.cs
+++ Infrastructure/Data/AplicationDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -11,5 +12,14 @@ namespace Infrastructure.Data
         public AplicationDbContext(DbContextOptions options) : base(options)
         {
         }
+
+         //public DbSet<Personaje> Personajes { get; set; }
+
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+
     }
 }
